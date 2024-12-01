571f9a883b8e9f54a25ed9fa4f05f7340f01be25
rayhan randi <rayhanrandi10@gmail.com>
2024-12-02 01:38:52 +0700

    feat(rag-warehouse): init required tables for rag

--- warehouse/migrations/1_init.sql
+++ warehouse/migrations/1_init.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS logs;
 CREATE DATABASE IF NOT EXISTS warehouse;
 
+-- configure log database/tables
 USE logs;
 
 CREATE TABLE IF NOT EXISTS log_data (
@@ -14,4 +15,31 @@ CREATE TABLE IF NOT EXISTS log_data (
 ) ENGINE = MergeTree()
 ORDER BY timestamp;
 
-# TODO: create table for processed data
+-- configure data warehouse database/tables
+USE warehouse;
+
+-- processed data
+CREATE TABLE IF NOT EXISTS analysis (
+    timestamp DateTime,
+    sensor Int64,
+    temp Float64,
+    hum Float64,
+    gyro_x Float64,
+    gyro_y Float64,
+    gyro_z Float64,
+    accel_x Float64,
+    accel_y Float64,
+    accel_z Float64,
+    cluster Int64,
+    prediction Int64,
+    PRIMARY KEY (timestamp)
+) ENGINE = MergeTree()
+ORDER BY timestamp;
+
+-- rag summaries of recent data
+CREATE TABLE IF NOT EXISTS rag (
+    timestamp DateTime,
+    summary String,
+    PRIMARY KEY (timestamp)
+) ENGINE = MergeTree()
+ORDER BY timestamp;
