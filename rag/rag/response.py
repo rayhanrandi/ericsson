d8a7e01004d2e7ff4027537bed09689a071f7ffc
abbymarvel <abbymarvel97@gmail.com>
2024-12-10 00:39:54 +0700

    feat(rag): impl streamlit dashboard for embedded grafana chatbot

--- rag/rag/response.py
+++ rag/rag/response.py
@@ -19,8 +19,8 @@ class LLMResponse:
         answer_prompt: PromptTemplate,
         llm_query: LLMQuery
     ):
-       self.answer_prompt = answer_prompt
-       self.llm_query = llm_query
+        self.answer_prompt = answer_prompt
+        self.llm_query = llm_query
 
     def get_response(self, question: str):
         chain = (
@@ -31,10 +31,16 @@ class LLMResponse:
             | self.llm_query.llm
             | StrOutputParser()
         )
-        response = chain.invoke({
-            "top_k": None,
-            "question": question,
-            "table_info": get_env_value("CLICKHOUSE_ANALYSIS_TABLE")
-        })
+        try:
+            response = chain.invoke({
+                "top_k": None,
+                "question": question,
+                "table_info": get_env_value("CLICKHOUSE_ANALYSIS_TABLE")
+            })
+        except Exception as e:
+            # Log the error if needed for debugging
+            print(f"Error occurred: {e}")
+            # Fallback response
+            response = "There is not enough data to analyze."
+
         return response
-       
